fca7edef7042d5cf7b8e0a3164f329f8c51fd11b
coles2hartwick <56162677+coles2hartwick@users.noreply.github.com>
2020-02-06 09:05:04 -0500

    Create String.py

---
+++ String.py
@@ -0,0 +1,23 @@
+# 2/6/19
+# Sam Cole
+
+response = input("Hello welcome to string practice please enter some words with commas in between each word")
+
+print(response.__len__())
+
+print(response[0])
+
+print(response[response.__len__() - 1])
+
+print(response.upper())
+
+print(response.title())
+
+print(response.isalpha())
+
+print(response.split(','))
+
+print(response.replace('a', 'e'))
+
+
+
